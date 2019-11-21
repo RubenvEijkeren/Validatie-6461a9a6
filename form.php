cd38234a71459e5b096057ff1c11a1d575bbd3ec
RubenvEijkeren <54809240+RubenvEijkeren@users.noreply.github.com>
2019-11-21 12:00:28 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>form</title>
+</head>
+<body>
+	<form method="POST">
+		<input type="text" name="email" placeholder="Type your email here">
+		<input type="submit" name="submit" value="Submit">
+	</form>
+	<?php
+		if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL))
+			echo $_POST["email"] . " is a valid email address";
+		else
+			echo $_POST["email"] . " is not a valid email address";
+	?>
+</body>
+</html>
